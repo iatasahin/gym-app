a4300105e4b153c48a91a56ea6aa7f3442f70330
İlker Ata Şahin <ilkeratasahin@gmail.com>
2026-01-02 15:58:16 +0300

    task: create TraineeDAO

---
+++ src/main/java/dev/ilkersahin/java/spring/gym/dao/TraineeDAO.java
@@ -0,0 +1,14 @@
+package dev.ilkersahin.java.spring.gym.dao;
+
+import dev.ilkersahin.java.spring.gym.model.Trainee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TraineeDAO {
+    Optional<Trainee> getTrainee(String userName);
+    List<Trainee> getAllTrainees();
+    Trainee createTrainee(Trainee trainee);
+    Trainee updateTrainee(Trainee trainee);
+    Optional<Trainee> deleteTrainee(Trainee trainee);
+}
