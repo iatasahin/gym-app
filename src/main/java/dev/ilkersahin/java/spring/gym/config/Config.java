5b579eaeae3ef9443c9afdb9e91aa776ef664173
İlker Ata Şahin <ilkeratasahin@gmail.com>
2026-01-11 18:26:17 +0300

    task: configure application.properties

---
+++ src/main/java/dev/ilkersahin/java/spring/gym/config/Config.java
@@ -0,0 +1,9 @@
+package dev.ilkersahin.java.spring.gym.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class Config {
+}
