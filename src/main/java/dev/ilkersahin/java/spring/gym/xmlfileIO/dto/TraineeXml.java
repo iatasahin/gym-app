fdff8c4195181f12676c576bf2f6102c4b38bc31
İlker Ata Şahin <ilkeratasahin@gmail.com>
2026-01-11 18:30:13 +0300

    task: create DTOs for model objects, for boundary separation and keeping model clear from @XML annotations

---
+++ src/main/java/dev/ilkersahin/java/spring/gym/xmlfileIO/dto/TraineeXml.java
@@ -0,0 +1,87 @@
+package dev.ilkersahin.java.spring.gym.xmlfileIO.dto;
+
+import dev.ilkersahin.java.spring.gym.xmlfileIO.adapter.LocalDateAdapter;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TraineeXml {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean isActive;
+
+    @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    private LocalDate dateOfBirth;
+    private String address;
+    private UUID userId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+}
