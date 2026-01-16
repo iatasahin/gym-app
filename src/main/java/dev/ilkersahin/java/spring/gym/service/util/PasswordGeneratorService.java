167a6cacdcd7f8550c7dcafffa5449e4365bd562
İlker Ata Şahin <ilkeratasahin@gmail.com>
2026-01-16 12:59:19 +0300

    task: PasswordGeneratorService: throw Exception for negative password lengths.

--- src/main/java/dev/ilkersahin/java/spring/gym/service/util/PasswordGeneratorService.java
+++ src/main/java/dev/ilkersahin/java/spring/gym/service/util/PasswordGeneratorService.java
@@ -18,6 +18,11 @@ public class PasswordGeneratorService {
     private SecureRandom secureRandom = new SecureRandom();
 
     public String generate(int length) {
+        if (length < 0) {
+            log.warn("PasswordGeneratorService::generate called with negative length: {}", length);
+            throw new IllegalArgumentException("Password length cannot be negative");
+        }
+
         log.debug("Generating password of length {}", length);
 
         StringBuilder sb = new StringBuilder(length);
