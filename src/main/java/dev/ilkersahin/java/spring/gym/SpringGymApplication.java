4fd7e7aadad19938def9c1b7497bec908544836c
İlker Ata Şahin <ilkeratasahin@gmail.com>
2026-03-17 20:40:51 +0300

    feature(Eureka): Make main app Eureka client, Create Eureka server, fix docker-compose.yml

--- src/main/java/dev/ilkersahin/java/spring/gym/SpringGymApplication.java
+++ src/main/java/dev/ilkersahin/java/spring/gym/SpringGymApplication.java
@@ -2,7 +2,9 @@ package dev.ilkersahin.java.spring.gym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SpringGymApplication {
     public static void main(String[] args) {
